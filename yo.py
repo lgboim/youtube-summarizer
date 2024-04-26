63ccb30d4f2a465863a96df6096a6c504a9f9674
Arielboim <lgboim@gmail.com>
2024-04-26 14:01:27 +0300

    Update yo.py

--- yo.py
+++ yo.py
@@ -121,8 +121,8 @@ def main():
         initial_sidebar_state="expanded"
     )
 
-    with open("style.css") as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    #with open("style.css") as f:
+        #st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
     st.title("Content Summarizer")
     
